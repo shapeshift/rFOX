d7fc2b2565a536d08e531992168d8c4ad3c63b81
woodenfurniture <125113430+woodenfurniture@users.noreply.github.com>
2024-04-05 12:57:27 +1100

    chore: scrappy typescript implementation of rewards distribution spreadsheet

---
+++ scripts/rewards-distribution/events.ts
@@ -0,0 +1,43 @@
+import { AbiEvent, Address, Log, parseEventLogs } from 'viem'
+
+type StakingEventName = 'Stake' | 'Unstake'
+
+type StakingAbiEvent<T extends StakingEventName> = {
+  type: 'event',
+  anonymous: false,
+  inputs: [
+    {
+      name: 'account',
+      type: 'address',
+      indexed: true
+    },
+    {
+      name: 'amount',
+      type: 'uint256',
+      indexed: false
+    }
+  ],
+  name: T
+}
+
+type GenericStakingEventLog<T extends StakingEventName> = Log<bigint, number, false, AbiEvent, true, StakingAbiEvent<T>[], T>
+
+// explicit union of all possible event logs to ensure event args are correctly parsed by ts (fixes defaulting to unknown)
+export type StakingEventLog = GenericStakingEventLog<'Stake'> | GenericStakingEventLog<'Unstake'>
+
+const addressA: Address = '0xA'
+const addressB: Address = '0xB'
+const addressC: Address = '0xC'
+const addressD: Address = '0xD'
+const addressE: Address = '0xE'
+
+export type StakingLog = Pick<StakingEventLog, 'blockNumber'|'eventName'|'args'>
+
+export const logs: StakingLog[] = [
+  { blockNumber: 20n, eventName: 'Stake', args: { account: addressA, amount: 100n } },
+  { blockNumber: 25n, eventName: 'Stake', args: { account: addressB, amount: 150n } },
+  { blockNumber: 32n, eventName: 'Stake', args: { account: addressC, amount: 10000n } },
+  { blockNumber: 33n, eventName: 'Stake', args: { account: addressD, amount: 1200n } },
+  { blockNumber: 60n, eventName: 'Unstake', args: { account: addressA, amount: 100n } },
+  { blockNumber: 65n, eventName: 'Stake', args: { account: addressE, amount: 500n } },
+]
--- scripts/rewards-distribution/index.ts
+++ scripts/rewards-distribution/index.ts
@@ -1,1 +1,93 @@
-console.log('hello world')
+import { Address } from 'viem'
+import { StakingLog, logs } from './events'
+import { assertUnreachable } from './helpers'
+
+const getStakingAmount = (log: StakingLog): bigint => {
+  switch (log.eventName) {
+    case 'Stake':
+      return log.args.amount
+    case 'Unstake':
+      return -log.args.amount
+    default:
+      assertUnreachable(log.eventName)
+  }
+
+  throw Error('should be unreachable')
+}
+
+// get the epoch and block reward for a given block number
+// TODO: this is a placeholder function matching the spreadsheet logic
+const getEpochBlockReward = (_epochEndBlockNumber: bigint) => {
+  // TODO: blockReward is calculated as half the total rune earned by the DAO divided by the number of blocks in the epoch
+  return 10n
+}
+
+// get the block range for the current epoch
+const getEpochBlockRange = () => {
+  const previousEpochEndBlockNumber = 0n
+  const currentBlockNumber = 100n
+  return { fromBlockNumber: previousEpochEndBlockNumber, toBlockNumber: currentBlockNumber }
+}
+
+// TODO: this should only process 1 epoch at a time
+const main = () => {
+  // index logs by block number
+  const logsByBlockNumber = logs.reduce<Record<string, StakingLog[]>>((acc, log) => {
+    if (!acc[log.blockNumber.toString()]) {
+      acc[log.blockNumber.toString()] = []
+    }
+    acc[log.blockNumber.toString()].push(log)
+    return acc
+  }, {})
+
+  // TODO: these will be initialized from the last epoch's state
+  let totalStaked = 0n
+  const balanceByAccount: Record<Address, bigint> = {}
+
+  // this must be initialized to empty
+  const epochRewardByAccount: Record<Address, number> = {}
+
+  // iterate all blocks for the current epoch
+  const { fromBlockNumber, toBlockNumber } = getEpochBlockRange()
+
+  const epochBlockReward = getEpochBlockReward(toBlockNumber)
+
+  for (let blockNumber = fromBlockNumber; blockNumber <= toBlockNumber; blockNumber++) {
+    const incomingLogs: StakingLog[] | undefined = logsByBlockNumber[blockNumber.toString()]
+
+    // process logs if there are any
+    if (incomingLogs !== undefined) {
+      for (const log of incomingLogs) {
+        const account = log.args.account
+        if (!balanceByAccount[account]) {
+          balanceByAccount[account] = 0n
+        }
+        const stakingAmount = getStakingAmount(log)
+        balanceByAccount[account] += stakingAmount
+        totalStaked += stakingAmount
+
+        // clear empty balances
+        if (balanceByAccount[account] === 0n) {
+          delete balanceByAccount[account]
+        }
+      }
+    }
+
+    for (const account of Object.keys(balanceByAccount) as Address[]) {
+      // calculate rewards for the current block
+      // TODO: Bignumber math should be used here to allow for more precision with floating point numbers
+      const proportionOfTotalStaked = totalStaked > 0n ? Number(balanceByAccount[account]) / Number(totalStaked) : 0
+      const reward = Number(epochBlockReward) * proportionOfTotalStaked
+
+      if (epochRewardByAccount[account] == undefined) {
+        epochRewardByAccount[account] = 0
+      }
+      epochRewardByAccount[account] += reward
+    }
+  }
+
+  console.log('rewards to be distributed:')
+  console.log(epochRewardByAccount)
+}
+
+main()

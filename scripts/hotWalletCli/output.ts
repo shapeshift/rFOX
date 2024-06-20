9849eb6ad5d7d18725d6f3c6a304193861a497f0
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2024-06-20 15:18:40 -0600

    add cli logging output helpers

--- scripts/hotWalletCli/output.ts
+++ scripts/hotWalletCli/output.ts
@@ -1,9 +1,13 @@
 import chalk from 'chalk'
 
 export const info = (text: string) => {
-  console.log(chalk.yellow('* ') + chalk.dim.yellow(text))
+  console.log(chalk.white('- ') + chalk.dim.white(text))
+}
+
+export const warn = (text: string) => {
+  console.log(chalk.yellow('* ') + chalk.yellow(text))
 }
 
 export const error = (text: string) => {
-  console.log(chalk.red('! ') + chalk.red(text))
+  console.log(chalk.red('! ') + chalk.bold.red(text))
 }

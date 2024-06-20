64b97b60f3c4c925152b37e9afd0450e3abef4a0
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2024-06-20 15:22:49 -0600

    update index.ts

--- scripts/hotWalletCli/index.ts
+++ scripts/hotWalletCli/index.ts
@@ -1,13 +1,18 @@
+import fs from 'node:fs'
+import path from 'node:path'
 import * as prompts from '@inquirer/prompts'
-import { create, recoverKeystore, KEYSTORE_FILE_PATH } from './mnemonic.js'
-import { info, error } from './output.js'
+import { create, recoverKeystore } from './mnemonic.js'
+import { info, error, warn } from './logging.js'
+import { createWallet, fund } from './wallet.js'
+import { RFOX_DIR } from './constants.js'
 
-async function run() {
-  const { mnemonic, keystore } = await create()
+const run = async () => {
+  const { mnemonic, keystoreFile: keystore } = await create()
 
   info(`Encrypted keystore file created (${keystore})`)
   info('Please back up your mnemonic in another secure way in case keystore file recovery fails!')
   info(`Mnemonic: ${mnemonic}`)
+  warn('DO NOT INTERACT WITH THIS WALLET FOR ANY REASON OUTSIDE OF THIS SCRIPT!')
 
   const confirmed = await prompts.confirm({
     message: 'Have you securely backed up your mnemonic? ',
@@ -17,15 +22,42 @@ async function run() {
     error('Unable to proceed knowing you have not securely backed up your mnemonic, exiting.')
     process.exit(1)
   }
+
+  const wallet = await createWallet(mnemonic)
+
+  // TODO: get total amount from distribution file (total distribution + fees to pay for all transactions)
+  const amount = '1'
+
+  await fund(wallet, amount)
 }
 
-async function recover() {
-  const mnemonic = await recoverKeystore(KEYSTORE_FILE_PATH)
+const recover = async () => {
+  const keystore = path.join(RFOX_DIR, 'keystore.txt')
+  const mnemonic = await recoverKeystore(keystore)
+  const wallet = await createWallet(mnemonic)
 
-  console.log({ mnemonic })
+  // TODO: get total amount from distribution file (total distribution + fees to pay for all transactions)
+  const amount = '1'
+
+  await fund(wallet, amount)
+}
+
+const shutdown = () => {
+  console.log()
+  warn('Received shutdown signal, exiting.')
+  process.exit(0)
 }
 
 const main = async () => {
+  try {
+    fs.mkdirSync(RFOX_DIR)
+  } catch (err) {
+    if (err instanceof Error) {
+      const fsError = err as NodeJS.ErrnoException
+      if (fsError.code !== 'EEXIST') throw err
+    }
+  }
+
   const choice = await prompts.select<'run' | 'recover'>({
     message: 'What do you want to do?',
     choices: [
@@ -52,5 +84,8 @@ const main = async () => {
       process.exit(1)
   }
 }
+
+process.on('SIGINT', shutdown)
+process.on('SIGTERM', shutdown)
 
 main()
